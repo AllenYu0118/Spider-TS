8de97a8bc4ad510f287e6e7fd82017db94200d93
yulei <yulei@addcn.com>
2020-06-07 22:28:53 +0800

    feat: add login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import router from './router'
 
 
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(router)
 
 app.listen(7001, () => {
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 
 import Crowller from './crowller'
 import DellAnalyzer from './dellAnalyzer'
@@ -7,16 +7,33 @@ const router = Router()
 
 
 router.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(`
+        <html>
+            <body>
+                <form method="post" action="/getData">
+                    <input type="password" name="password">
+                    <button>提交</button>
+                </form>
+            </body>
+        </html>
+    `)
 })
 
-router.get('/getData', (req, res) => {
-    const secret = 'soga9527'
-    const url = `http://www.dell-lee.com/?secret=${secret}`
-    const analyzer = DellAnalyzer.getInsatance()
+router.get('/login')
 
-    new Crowller(url, analyzer)
-    res.send('getData Success!')
+router.post('/getData', (req: Request, res: Response) => {
+    console.log(req.body)
+    if (req.body.password === '123') {
+        const secret = 'soga9527'
+        const url = `http://www.dell-lee.com/?secret=${secret}`
+        const analyzer = DellAnalyzer.getInsatance()
+
+        new Crowller(url, analyzer)
+        res.send('getData Success!')
+    } else {
+        res.send('password Error!')
+    }
+
 })
 
 export default router
