a46a2e2be1e8f0911e4ddb9b32b3a7864ccafeed
余磊 <yulei@addcn.com>
2020-11-04 00:20:02 +0800

    feat: add mysql config

---
+++ backend/src/db/index.ts
@@ -0,0 +1,29 @@
+import mysql from "mysql"
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'spider_db'
+})
+
+connection.connect(err => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+})
+
+connection.query('SELECT 1', function (error, results, fields) {
+  if (error) throw error;
+  console.log('connected!')
+});
+
+export default connection
+
+// connection.query('CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), email VARCHAR(255))', (err, result) => {
+//     if (err) throw err;
+//     console.log('Table Create Success!')
+// })
